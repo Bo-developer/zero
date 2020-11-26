39b93f68aecc5d089d3aa1362162ea76eaa5c61b
Bobrovskyi Dmytro <dmytro.bobrovskyi@dedicatedlab.com>
2020-11-26 15:20:54 +0200

    Add open graph

--- gulp/tasks/pug2html.js
+++ gulp/tasks/pug2html.js
@@ -6,7 +6,7 @@ const htmlValidator = require('gulp-w3c-html-validator')
 module.exports = function pug2html() {
     return gulp.src('src/pages/*.pug')
         .pipe(plumber())
-        .pipe(pug())
+        .pipe(pug({pretty: true}))
         .pipe(htmlValidator())
         .pipe(gulp.dest('build'))
 }
